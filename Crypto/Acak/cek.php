81a5b8b0eec6c5a8885da54f73461c4fc33d8f72
lan666as <4zidan@gmail.com>
2021-08-14 18:28:41 +0700

    update soal

--- Crypto/Acak/cek.php
+++ Crypto/Acak/cek.php
@@ -2,6 +2,10 @@
 include("flag.php");
 session_start();
 
+if(!isset($_SESSION["state"])){
+    header('Location: https://acak.ctf-kmteti.engineer/');
+}
+
 $state= $_SESSION["state"];
 $m = $_SESSION["m"];
 $c = $_SESSION["c"];
@@ -11,8 +15,13 @@ if(!isset($_GET["tebak"])){
     show_source(__FILE__);
 }
 else {
-    if(($_GET["tebak"] == ($state*$a+$c)%$m) && (time() - $_SESSION["start_time"] <= 2)){
-        echo "Selamat lur : " . $flag;
+    if(is_numeric($_GET["tebak"])){
+        if(($_GET["tebak"] == ($state*$a+$c)%$m) && (time() - $_SESSION["start_time"] <= 2)){
+            echo "Selamat lur : " . $flag;
+        }
+        else{
+            echo "Coba lagi gan";
+        }
     }
     else{
         echo "Coba lagi gan";
